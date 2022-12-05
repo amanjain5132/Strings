18e3eea8c17e475f8399afdbd3e8938fe4434380
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-12-05 16:21:46 +0530

    Create BASIC1.java
    Write a basic program to take input (String) from User and just print it

---
+++ BASIC1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the string you want to print");
+	    String s1 = sc.next();
+	    System.out.println(s1);
+	}
+}
